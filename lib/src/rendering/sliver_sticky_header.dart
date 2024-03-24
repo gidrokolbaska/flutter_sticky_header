0e6aad9bd57fb4652aa06f22d697d81789f221a3
Andrey Ilyichev <gidroskolbasovich@gmail.com>
2024-03-24 22:51:25 +0300

    test

--- lib/src/rendering/sliver_sticky_header.dart
+++ lib/src/rendering/sliver_sticky_header.dart
@@ -256,7 +256,7 @@ class RenderSliverStickyHeader extends RenderSliver with RenderSliverHelpers {
                   constraints.viewportMainAxisExtent);
 
       final double headerScrollRatio =
-          ((headerPosition - constraints.overlap).abs() / _headerExtent!);
+          ((headerPosition - 10 - constraints.overlap).abs() / _headerExtent!);
       if (_isPinned && headerScrollRatio <= 1) {
         controller?.stickyHeaderScrollOffset =
             constraints.precedingScrollExtent;
@@ -318,7 +318,8 @@ class RenderSliverStickyHeader extends RenderSliver with RenderSliverHelpers {
       final didHitHeader = hitTestBoxChild(
         BoxHitTestResult.wrap(SliverHitTestResult.wrap(result)),
         header!,
-        mainAxisPosition: mainAxisPosition - childMainAxisPosition(header) - headerPosition,
+        mainAxisPosition:
+            mainAxisPosition - childMainAxisPosition(header) - headerPosition,
         crossAxisPosition: crossAxisPosition,
       );
 
